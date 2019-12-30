68ee71396feca85b538f21e8eddc5b3ab29b7344
Josué Girón <benjamingiron1@gmail.com>
2019-12-30 14:35:01 -0600

    Crear funcion para agregar dispositivo a one signal

--- functions.go
+++ functions.go
@@ -1,4 +1,19 @@
 package jonesignal
 
+import "errors"
+
 // AddDevice ...
-func AddDevice() {}
+func AddDevice(data OSAddDeviceReq) (string, error) {
+
+	response, err := addNewDevice(data)
+	if err != nil {
+		return "", err
+	}
+
+	if !response.Success {
+		return "", errors.New("No pudo agregar el dispositivo")
+	}
+
+	return response.ID, nil
+
+}
